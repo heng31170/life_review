cf7aa4109bbc9d334dcc4f8291a402c7f4c418df
heng31170 <18655416913@163.com>
2025-02-11 22:50:42 +0800

    第十三次提交，基于Stream消息队列实现异步秒杀

--- src/main/resources/seckill.lua
+++ src/main/resources/seckill.lua
@@ -2,6 +2,8 @@
 local voucherId = ARGV[1]
 -- 用户id
 local userId = ARGV[2]
+-- 订单id
+local orderId = ARGV[3]
 -- 库存key
 local stockKey = 'seckill:stock:' .. voucherId
 -- 订单key
@@ -20,4 +22,6 @@ end
 redis.call('incrby', stockKey, -1)
 -- 将userId存入当前优惠券的set集合
 redis.call('sadd', orderKey, userId)
+-- 将下单数据保存到消息队列中  XADD stream.order * k1 v1 k2 v2 ...
+redis.call("xadd", 'stream.orders', '*', 'userId', userId, 'voucherId', voucherId, 'id', orderId)
 return 0
